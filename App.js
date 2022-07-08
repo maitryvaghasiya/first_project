6604ebb0deb26abb78fd86693488df84d02b929e
maitryvaghasiya <maitryvaghasiya2426@gmail.com>
2022-07-08 09:10:38 +0530

    p1

--- App.js
+++ App.js
@@ -4,8 +4,52 @@ import React from 'react'
 export default function App() {
 
   let data = [
-    
-  ]
+    {
+      id: 101,
+      name: 'Abacavir',
+      quantity: 25,
+      price: 150,
+      expiry: 2022,
+      status: true
+    },
+    {
+      id: 102,
+      name: 'Eltrombopag',
+      quantity: 90,
+      price: 550,
+      expiry: 2021,
+      status: true
+    },
+    {
+      id: 103,
+      name: 'Meloxicam',
+      quantity: 85,
+      price: 450,
+      expiry: 2025,
+      status: false
+    },
+    {
+      id: 104,
+      name: 'Allopurinol',
+      quantity: 50,
+      price: 600,
+      expiry: 2023,
+      status: true
+    },
+    {
+      id: 105,
+      name: 'Phenytoin',
+      quantity: 63,
+      price: 250,
+      expiry: 2021,
+      status: false
+    }
+  ];
+
+
+  let x = data.filter((id) => (id.expiry>=2022));
+  console.log(x);
+  
   return (
     <View>
       <Text>App</Text>
